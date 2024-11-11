ffe3fbb39bc43f877757ae9270c616654e68a5df
connor <nenwam333@gmail.com>
2024-11-11 11:20:21 -0800

    initial public commit

--- src/App.js
+++ src/App.js
@@ -14,6 +14,48 @@ const monday = mondaySdk();
 // monday.setToken("eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjI3Mjk5MDQ5NiwiYWFpIjoxMSwidWlkIjozNjI5NTI0NywiaWFkIjoiMjAyMy0wOC0wM1QyMToyMjozNy4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTI3MTA0ODYsInJnbiI6InVzZTEifQ.XIrSWOWgg3U7oRd9zrKzL0WAr8Peo5b4ZIU1vfw0T2w");
 const storageInstance = monday.storage.instance;
 
+// const [token, setToken] = useState(null);
+
+  // useEffect(() => {
+  //   // Check if token is already stored
+  //   const storedToken = sessionStorage.getItem("mondayToken");
+  //   if (storedToken) {
+  //     setToken(storedToken);
+  //     monday.setToken(storedToken);
+  //   } else {
+  //     // Redirect to OAuth authorization URL
+  //     window.location.href = `https://auth.monday.com/oauth2/authorize?client_id=d607d4ff104e14f6d98be7ce2d27483b&redirect_uri=YOUR_REDIRECT_URI&response_type=code`;
+  //   }
+  // }, []);
+
+  // useEffect(() => {
+  //   // Handle OAuth callback
+  //   const urlParams = new URLSearchParams(window.location.search);
+  //   const code = urlParams.get("code");
+  //   if (code && !token) {
+  //     // Exchange code for access token
+  //     fetch("https://auth.monday.com/oauth2/token", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({
+  //         client_id: "YOUR_CLIENT_ID",
+  //         client_secret: "YOUR_CLIENT_SECRET",
+  //         code: code,
+  //         redirect_uri: "YOUR_REDIRECT_URI",
+  //       }),
+  //     })
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         setToken(data.access_token);
+  //         sessionStorage.setItem("mondayToken", data.access_token);
+  //         monday.setToken(data.access_token);
+  //       })
+  //       .catch((error) => console.error("Error fetching token:", error));
+  //   }
+  // }, [token]);
+
 const App = () => {
   
   return (
